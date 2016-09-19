0fa54351c4b6276cbc2193c4d7a7743bdaffbf97
Subhash Kumar <jietss2011@gmail.com>
2016-09-19 15:11:31 +0530

    Initialize repository

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,24 @@
+class UrlsController < ApplicationController
+  def new
+    @shortened_url = Url.new
+  end
+
+  def create
+    @shortened_url = Url.new(new_params)
+    if @shortened_url.save
+      flash[:shortened_id] = @shortened_url.id
+      redirect_to new_url_url
+    else
+      render :action => "new"
+    end
+  end
+ def show
+  @shortened_url=Url.find(params[:id])
+redirect_to @shortened_url.url
+end
+ private
+   
+ def new_params
+  params.require(:url).permit(:url)
+ end
+end
